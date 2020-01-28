edee6f7b016f137666cb1ef80cd126ba20dfd083
Vikram Ingawale <vikram.ingawale91@gmail.com>
2020-01-28 22:35:34 +0530

    Code Refator

--- spinners/spinner.go
+++ spinners/spinner.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"os"
 	"time"
 )
 
@@ -12,44 +10,61 @@ import (
 type spinner struct {
 	SpinnerName     string
 	SpinnerInterval int
+	SpinnerDuration time.Duration
 	SpinnerParts    []string
 }
 
-// spinnerInfo - spinnerInfo struct
-type spinnerInfo struct {
+// SpinnerInfo - SpinnerInfo struct
+type SpinnerInfo struct {
 	spinner spinner
-	out     io.Writer
+	// out     io.Writer
 	running bool
 	done    context.CancelFunc
 }
 
 func main() {
 	// getSpinner - get spinner
-	s := getSpinner()
-	o := os.Stdout
-	// assign values to spinnerInfo
-	l := &spinnerInfo{
-		spinner: s,
-		out:     o,
-	}
-	// start the spinner
-	l.startSpinner()
+	// s := getSpinner()
+	// o := os.Stdout
+	// // assign values to SpinnerInfo
+	// l := &SpinnerInfo{
+	// 	spinner: s,
+	// 	out:     o,
+	// }
+	// // start the spinner
+	// l.startSpinner()
 }
 
 // getSpinner - getSpinner
-func getSpinner() spinner {
-	return spinner{
-		SpinnerName:     "ArrowSpinner",
-		SpinnerInterval: 50, // add interval
-		SpinnerParts:    []string{`←`, `↖`, `↑`, `↗`, `→`, `↘`, `↓`, `↙`},
+func getSpinner(spinnerName string, duration time.Duration) spinner {
+	switch spinnerName {
+	case "ArrowSpinner":
+		return spinner{
+			SpinnerName:     "ArrowSpinner", // "ArrowSpinner"
+			SpinnerInterval: 50,             // add interval
+			SpinnerDuration: duration,
+			SpinnerParts:    []string{`←`, `↖`, `↑`, `↗`, `→`, `↘`, `↓`, `↙`},
+		}
 	}
+	return spinner{}
 }
 
-// startSpinner - start Spinner
-func (l *spinnerInfo) startSpinner() {
+// New spinner
+func New(spinnerName string, duration time.Duration) *SpinnerInfo {
+	s := getSpinner(spinnerName, duration)
+	// o := os.Stdout
+	l := SpinnerInfo{
+		spinner: s,
+		// out:     o,
+	}
+	return &l
+}
+
+// startSpinner - start spinner
+func (l *SpinnerInfo) startSpinner() {
 	if !l.running {
-		ctx, done := context.WithCancel(context.Background()) // WithCancel
-		// ctx, done := context.WithTimeout(context.Background(), time.Second*10) // withtimeout
+		// ctx, done := context.WithCancel(context.Background()) // WithCancel
+		ctx, done := context.WithTimeout(context.Background(), time.Second*5) // withtimeout
 		l.done = done
 		l.running = true
 		// call to print spinner
@@ -58,24 +73,25 @@ func (l *spinnerInfo) startSpinner() {
 }
 
 // printSpinner - print spinner
-func (l *spinnerInfo) printSpinner(ctx context.Context) {
+func (l *SpinnerInfo) printSpinner(ctx context.Context) {
 	t := time.NewTicker(time.Duration(l.spinner.SpinnerInterval) * time.Millisecond)
 	n := 0
 	for {
 		select {
 		case <-ctx.Done():
-			t.Stop()       // stop : to release associated resources
+			t.Stop() // stop : to release associated resources
 			return
 		case <-t.C:
-			part := "\r" + l.spinner.SpinnerParts[n%len(l.spinner.SpinnerParts)] + " loading"
-			fmt.Fprint(l.out, part)
+			// part := "\r" + l.spinner.SpinnerParts[n%len(l.spinner.SpinnerParts)] + " loading"
+			// fmt.Fprint(l.out, part)
+			fmt.Printf("\r%s", l.spinner.SpinnerParts[n%len(l.spinner.SpinnerParts)]+" loading")
 			n++
 		}
 	}
 }
 
 // Stop - stop is ticker struct method
-func (l *spinnerInfo) Stop() {
+func (l *SpinnerInfo) Stop() {
 	if l.done != nil {
 		l.done()
 	}
